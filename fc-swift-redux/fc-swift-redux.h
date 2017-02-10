cf37fe3cb1bfd9eb55cafa66628b7c98a460817d
Fredrik <fredrik.christenson@gmail.com>
2017-02-10 19:46:52 +0100

    init

---
+++ fc-swift-redux/fc-swift-redux.h
@@ -0,0 +1,19 @@
+//
+//  fc-swift-redux.h
+//  fc-swift-redux
+//
+//  Created by Fredrik Christenson on 2017-02-10.
+//  Copyright © 2017 Fredrik Christenson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for fc-swift-redux.
+FOUNDATION_EXPORT double fc_swift_reduxVersionNumber;
+
+//! Project version string for fc-swift-redux.
+FOUNDATION_EXPORT const unsigned char fc_swift_reduxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <fc_swift_redux/PublicHeader.h>
+
+
